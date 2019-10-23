d024e216a20402bc22c35fae892464ce82406ae3
Andrei <apanaligan@you-source.com>
2019-10-23 20:26:56 +0800

    Changed stepName for prospectFieldEquals step

--- src/steps/prospect/prospect-field-equals.ts
+++ src/steps/prospect/prospect-field-equals.ts
@@ -4,7 +4,7 @@ import { Step, RunStepResponse, FieldDefinition, StepDefinition } from '../../pr
 // tslint:disable:no-else-after-return
 export class ProspectFieldEquals extends BaseStep implements StepInterface {
 
-  protected stepName: string = 'Validate a Pardot Prospect field';
+  protected stepName: string = 'Check a field on Pardot Prospect';
   protected stepExpression: string = 'the (?<field>[a-zA-Z0-9_]+) field on pardot prospect (?<email>.+) should be (?<expectedValue>.+)';
   protected stepType: StepDefinition.Type = StepDefinition.Type.VALIDATION;
   protected expectedFields: Field[] = [{
