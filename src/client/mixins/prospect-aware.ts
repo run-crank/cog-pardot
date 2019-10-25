c9e7cd009840591d7a56c65391f9e05ae40081a9
Andrei <apanaligan@you-source.com>
2019-10-25 12:18:27 +0800

    Implemented retry for concurrency error code

--- src/client/mixins/prospect-aware.ts
+++ src/client/mixins/prospect-aware.ts
@@ -3,12 +3,20 @@ import * as pardot from 'lew-pardot';
 export class ProspectAwareMixin {
   public client: pardot;
   public clientReady: Promise<boolean>;
+  public retry: any;
+
+  public attempt: (promise: Promise<any>, attemptCount?: number) => Promise<any>;
 
   public async createProspect(prospect: Record<string, any>) {
     await this.clientReady;
-    return new Promise((resolve, reject) => {
-      this.client.prospects.create(prospect.email, prospect).then(resolve).fail(reject);
+
+    const promise = new Promise((resolve, reject) => {
+      this.client.prospects.create(prospect.email, prospect)
+      .then(resolve)
+      .fail(reject);
     });
+
+    return this.attempt(promise, 5);
   }
 
   public async deleteProspectByEmail(email: string) {
@@ -16,15 +24,17 @@ export class ProspectAwareMixin {
 
     const prospect: any = await this.readByEmail(email);
 
-    return new Promise((resolve, reject) => {
+    const promise = new Promise((resolve, reject) => {
       this.client.prospects.deleteById(prospect.id).then(resolve).fail(reject);
     });
+
+    return this.attempt(promise);
   }
 
   public async readByEmail(email: string) {
     await this.clientReady;
 
-    return new Promise((resolve, reject) => {
+    const promise = new Promise((resolve, reject) => {
       this.client.prospects.readByEmail(email).then((response) => {
         const prospects = response.prospect;
 
@@ -37,5 +47,7 @@ export class ProspectAwareMixin {
 
       }).fail(reject);
     });
+
+    return this.attempt(promise);
   }
 }
