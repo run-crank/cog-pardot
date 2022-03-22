d6e9fb19b98b6841e0b66daa3272a9b34eb7739f
Austin Elwell <austinelwell99@gmail.com>
2022-03-22 14:56:05 -0500

    updated error messages

--- src/steps/prospect/prospect-field-equals.ts
+++ src/steps/prospect/prospect-field-equals.ts
@@ -98,6 +98,8 @@ export class ProspectFieldEquals extends BaseStep implements StepInterface {
         return this.error('There was an error checking the prospect field: %s', [e.message]);
       } else if (e?.response?.data?.err === 'Invalid prospect email address') {
         return this.fail('No prospect found with email %s', [email]);
+      } else if (e.toString().includes('Invalid value "undefined" for header "Pardot-Business-Unit-Id"')) {
+        return this.error('There was a problem deleting the Prospect: No Prospect found with email %s in Business Unit %s', [email, buidName]);
       }
       return this.error('There was an error checking the prospect field: %s', [e.message]);
     }
